b3fac8ec17dc87ed857e05bc93c16dd17e5712fa
Mhvsl98 <m4vslofc@gmail.com>
2024-08-24 00:00:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 /* Desenvolva seu código abaixo */
+
+function playRockPaperScissor(player1, player2) {
+  player1 = player1.toLowerCase();
+  player2 = player2.toLowerCase();
+
+  if (
+    (player1 === "pedra" && player2 === "tesoura") ||
+    (player1 === "papel" && player2 === "pedra") ||
+    (player1 === "tesoura" && player2 === "papel")
+  ) {
+    return "Jogador 1 venceu!";
+  } else if (
+    (player2 === "pedra" && player1 === "tesoura") ||
+    (player2 === "papel" && player1 === "pedra") ||
+    (player2 === "tesoura" && player1 === "papel")
+  ) {
+    return "Jogador 2 venceu!";
+  }
+  return "Empate!";
+}
+console.log(playRockPaperScissor("Pedra", "Tesoura"));
